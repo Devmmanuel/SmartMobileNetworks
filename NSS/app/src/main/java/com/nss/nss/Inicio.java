50ef9483cffbbebc089e63b5ec115a47544bdb34
orlando1818 <lopez.orlando.gold9@gmail.com>
2019-11-10 13:52:12 -0600

    Se agergo metodo para pedir los permisos nesesarios al iniciar la aplicacion

--- NSS/app/src/main/java/com/nss/nss/Inicio.java
+++ NSS/app/src/main/java/com/nss/nss/Inicio.java
@@ -1,9 +1,13 @@
 package com.nss.nss;
 
+import android.Manifest;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -11,7 +15,6 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageButton;
-import android.widget.Toast;
 
 
 public class Inicio extends AppCompatActivity implements View.OnClickListener {
@@ -28,6 +31,16 @@ public class Inicio extends AppCompatActivity implements View.OnClickListener {
 
         btnMenuRedesMoviles = findViewById(R.id.btnMovil);
         btnMenuRedesMoviles.setOnClickListener(this);
+        darPermisosApp();
+    }
+
+    /*este metodo sirve para dar permisos a las aplicacion ya que si no le damos los permios suficientes a la aplicacion no pedemos
+    * usar algunos metodos de PhoneManeger*/
+    public void darPermisosApp(){
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED)
+            ActivityCompat.requestPermissions
+                    (this,new String[]{Manifest.permission.READ_PHONE_STATE},
+                            123);
     }
 
     @Override
